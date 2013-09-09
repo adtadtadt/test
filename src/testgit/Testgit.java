a8b83d1ee7d6d5bcdc8cb63df927d8d794fb280a
adt <adt@L404P00.pdg.esi.heb.site>
2013-09-09 16:06:37 +0200

    Premier  dcfhdfhdfthrtyh

--- src/testgit/Testgit.java
+++ src/testgit/Testgit.java
@@ -14,6 +14,6 @@ public class Testgit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here ggg
+        // TODO code application logic here ggg dssd
     }
 }
